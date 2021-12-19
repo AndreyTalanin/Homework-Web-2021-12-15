23e6f3c0a779f32cab87e19e00d6dfd2a0c1bc92
Andrey Talanin <andrey.talanin@outlook.com>
2021-12-19 17:53:00 +0300

    Add a template for the RecipeDetailsPageComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { BrowserModule } from "@angular/platform-browser";
 
 import { AppComponent } from "./app.component";
 import { HomePageComponent } from "./home-page/home-page.component";
+import { RecipeDetailsPageComponent } from "./recipe-details-page/recipe-details-page.component";
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent],
+  declarations: [AppComponent, HomePageComponent, RecipeDetailsPageComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
