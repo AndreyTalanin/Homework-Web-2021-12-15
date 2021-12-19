0da5944e903269d3035e78d67da1ff14b88db612
Andrey Talanin <andrey.talanin@outlook.com>
2021-12-19 18:01:44 +0300

    Add a template for the ShoppingListPageComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from "@angular/platform-browser";
 import { AppComponent } from "./app.component";
 import { HomePageComponent } from "./home-page/home-page.component";
 import { RecipeDetailsPageComponent } from "./recipe-details-page/recipe-details-page.component";
+import { ShoppingListPageComponent } from "./shopping-list-page/shopping-list-page.component";
 
 @NgModule({
-  declarations: [AppComponent, HomePageComponent, RecipeDetailsPageComponent],
+  declarations: [AppComponent, HomePageComponent, RecipeDetailsPageComponent, ShoppingListPageComponent],
   imports: [BrowserModule],
   providers: [],
   bootstrap: [AppComponent],
