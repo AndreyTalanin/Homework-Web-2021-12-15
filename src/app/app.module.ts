b0b176c187ed5009a2cf3de0f6344c817fd21907
Andrey Talanin <andrey.talanin@outlook.com>
2021-12-19 20:16:15 +0300

    Implement routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from "@angular/platform-browser";
+import { RouterModule } from "@angular/router";
 
 import { AppComponent } from "./app.component";
 import { HomePageComponent } from "./home-page/home-page.component";
@@ -11,7 +12,15 @@ import { SideMenuComponent } from "./side-menu/side-menu.component";
 
 @NgModule({
   declarations: [AppComponent, HomePageComponent, RecipeDetailsPageComponent, ShoppingListPageComponent, SideMenuComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    RouterModule.forRoot([
+      { path: "home", component: HomePageComponent },
+      { path: "recipe-details/:id", component: RecipeDetailsPageComponent },
+      { path: "shopping-list", component: ShoppingListPageComponent },
+      { path: "", redirectTo: "/home", pathMatch: "full" },
+    ]),
+  ],
   providers: [RecipeRepositoryService, ShoppingListManagerService],
   bootstrap: [AppComponent],
 })
